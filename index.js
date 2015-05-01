9c31d4549789a64a9a52ce041f1bc2bacb7ef61f
psirenny <djtorres0@gmail.com>
2015-04-30 21:57:57 -0400

    include lodash

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+var _ = require('lodash');
 var UAParser = require('ua-parser-js');
 
-module.exports = function (app, opts) {
-  if (!opts) opts = {};
-  if (!opts.path) opts.path = '$useragent';
+module.exports = function (app, options) {
+  var defs = {path: '$useragent'};
+  var opts = _.merge({}, defs, options || {});
 
   app.get('*', function (page, model, params, next) {
     var parser = new UAParser();
     parser.setUA(page.req.headers['user-agent']);
     var result = parser.getResult();
-    for (key in result) {
-      model.set(opts.path + '.' + key, result[key]);
-    }
+    _.each(parser.getResult(), function (val, key) {
+      model.set(opts.path + '.' + key, val);
+    });
     next();
   });
 };
